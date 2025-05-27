fa98912e9d3481d70cbc489f411ed544f448099a
mrnoh99 <34833225+mrnoh99@users.noreply.github.com>
2025-05-27 14:20:37 +0900

    Initial Commit

---
+++ FindingColor/FindingColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindingColorApp.swift
+//  FindingColor
+//
+//  Created by NohJaisung on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct FindingColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
